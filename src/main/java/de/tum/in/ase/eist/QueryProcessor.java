8aa8de99b4ffa288189e2e6a824bc08a08d3e048
Moritz Dauer <ge28kel@mytum.de>
2022-07-13 13:18:45 +0200

    name

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -10,9 +10,8 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("plus")) {
-           String [] string = query.split(" ");
-           return String.valueOf((Integer.parseInt(string[2]) + Integer.parseInt(string[4])));
+        } else if (query.contains("name")) {
+           return "MoDe";
         } else { // TODO extend the programm here
             return "";
         }
