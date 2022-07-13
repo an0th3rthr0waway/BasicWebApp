6d610daecc9ce907724fddd6f9acd509711b4d22
Moritz Dauer <ge28kel@mytum.de>
2022-07-13 13:17:38 +0200

    what is plus

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -10,8 +10,9 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
-           return "438";
+        } else if (query.contains("plus")) {
+           String [] string = query.split(" ");
+           return String.valueOf((Integer.parseInt(string[2]) + Integer.parseInt(string[4])));
         } else { // TODO extend the programm here
             return "";
         }
